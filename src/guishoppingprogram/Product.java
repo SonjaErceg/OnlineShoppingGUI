850aec3b57b3735842a625bc13e03c9efff47662
Sonja Erceg <fmt9367@aut.ac.nz>
2025-10-30 14:56:14 +1300

    Update Product class to include id and description

--- src/guishoppingprogram/Product.java
+++ src/guishoppingprogram/Product.java
@@ -13,14 +13,20 @@ package guishoppingprogram;
 import java.awt.*;
 
 public class Product {
+    private int id;
     private String name;
     private double price;
+    private String description;
 
-    public Product(String name, double price) {
+    public Product(int id,String name, double price, String description) {
+        this.id = id;
         this.name = name;
         this.price = price;
+        this.description = description;
     }
-
+    
+    public int getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
+    public String getDescription() { return description; }
 }
